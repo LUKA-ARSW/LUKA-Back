8c7d6bddd1d9c198cd0e7e8d7831583df65a8aa1
LuisaGiron <luisa.bermudez@mail.escuelaing.edu.co>
2023-09-27 18:23:41 -0500

    Creacion proyecto

---
+++ src/main/java/edu/arsw/luka/lukaBack/LukaBackApplication.java
@@ -0,0 +1,13 @@
+package edu.arsw.luka.lukaBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LukaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LukaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/arsw/luka/lukaBack/LukaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.arsw.luka.lukaBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LukaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
