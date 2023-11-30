0fa49b8a1f7a95545e8e582fe1786ea1e983c4f4
LuisaGiron <luisa.bermudez@mail.escuelaing.edu.co>
2023-11-30 01:05:52 -0500

    servicios de configuracion de Redis

--- src/main/java/edu/arsw/luka/lukaBack/services/RedisAutorizacionServicio.java
+++ src/main/java/edu/arsw/luka/lukaBack/services/RedisAutorizacionServicio.java
@@ -1,6 +1,8 @@
 package edu.arsw.luka.lukaBack.services;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import edu.arsw.luka.lukaBack.exception.LukaNoAutorizadoException;
@@ -11,10 +13,12 @@ import edu.arsw.luka.lukaBack.util.WebToken;
 public class RedisAutorizacionServicio implements AutorizacionServicio{
 
     private final JsonWebToken jsonWebToken;
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Autowired
-    public RedisAutorizacionServicio(JsonWebToken jsonWebToken) {
+    public RedisAutorizacionServicio(JsonWebToken jsonWebToken, RedisTemplate<String, String> redisTemplate) {
         this.jsonWebToken = jsonWebToken;
+        this.redisTemplate = redisTemplate;
     }
 
     @Override
@@ -25,6 +29,9 @@ public class RedisAutorizacionServicio implements AutorizacionServicio{
 
     @Override
     public String crearToken(WebToken webToken) {
+        var token = jsonWebToken.createToken(webToken);
+        JSONObject jsonObject = new JSONObject(webToken.toString());
+        redisTemplate.opsForValue().set(jsonObject.getString("correo"),token);
         return jsonWebToken.createToken(webToken);
     }
     
